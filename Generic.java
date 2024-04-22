6ec3564cd6381b295a501dda3ac21cbc4c5d2403
MLHIPK <167212344+MLHIPK@users.noreply.github.com>
2024-04-22 13:49:26 +0300

    Update Generic.java

--- Generic.java
+++ Generic.java
@@ -1,15 +1,20 @@
+package org.odev10;
+
 public class Generic<T> {
     private  T content;
-    public T getContent()
-    {
+    public T getContent() {
         return content;
     }
-  public void setContent(T content) {
+
+    public void setContent(T content) {
         this.content = content;
     }
-   public void printContent() {
+
+    public void printContent() {
         System.out.println("Generic Content: " + content);
     }
-}
-   
-       
+
+    public void printSeparator() {
+        System.out.println("---------------------------------------------");
+    }
+}      
